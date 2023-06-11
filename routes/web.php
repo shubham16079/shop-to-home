8ad8d16adf570c99ccf88073819754009a75c47f
dazos_shubham <shubham@dazos.com>
2023-06-11 13:36:43 +0530

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hello');
 });
 Route::get('/testing', function () {
     return view('hello');
